0cd21c2a99e3ea1ac78cca9914a7f6bfd75290f6
OmriGanor <gaomri123@gmail.com>
2018-07-06 11:29:05 +0300

    added checker for same domain

--- code/engine.py
+++ code/engine.py
@@ -3,8 +3,10 @@ Author: Omri Ganor
 Purpose: Checks whether a website might be a phishing copy of another website.
 """
 from checkers.static_checkers.is_known_phish import is_known_phish
+from checkers.is_same_domain import is_same_domain
 
 
 def run_engine(original_url, test_url, config):
-    return is_known_phish(test_url, config["checkers"]["static_checkers"]["known_phish_database_path"])
+    #is_known_phish(test_url, config["checkers"]["static_checkers"]["known_phish_database_path"])
+    return is_same_domain(original_url, test_url)
 
