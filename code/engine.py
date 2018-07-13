6f977201886d7e8f1028e9bbe9eb41db65be1d19
OmriGanor <gaomri123@gmail.com>
2018-07-13 19:06:28 +0300

    using relative path for known_phish_database instaed of fullpath

--- code/engine.py
+++ code/engine.py
@@ -6,12 +6,12 @@ from checkers.absolute_checkers.is_same_domain import is_same_domain
 from checkers.absolute_checkers.is_known_phish import is_known_phish
 from checkers.is_long_url import is_long_url
 from checkers.is_similar_urls import is_similar_urls
+import os
 
 
 def run_engine(original_url, test_url, config):
-    is_known_phish(test_url, config["checkers"]["absolute_checkers"]["known_phish_database_path"])
-    is_same_domain(original_url, test_url)
-    is_long_url(test_url, config["checkers"]["long_url_length"])
-    return is_similar_urls(original_url, test_url)
-
-
+    print(is_known_phish(test_url, os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                                config["checkers"]["absolute_checkers"]["known_phish_database_path"])))
+    #is_same_domain(original_url, test_url)
+    #is_long_url(test_url, config["checkers"]["long_url_length"])
+    #return is_similar_urls(original_url, test_url)
