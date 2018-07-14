cc6390973da5801ab8e54688fe719605412231b2
OmriGanor <gaomri123@gmail.com>
2018-07-14 13:30:55 +0300

    added exception handling for checkers

--- code/checkers/absolute_checkers/is_known_phish.py
+++ code/checkers/absolute_checkers/is_known_phish.py
@@ -3,7 +3,7 @@ Author: Omri Ganor
 Purpose: Checks whether a url is in the known phishing DB.
 """
 
-from checkers.checker import Checker
+from checkers.checker import Checker, CheckFailedException
 
 
 class KnowPhishChecker(Checker):
@@ -12,12 +12,14 @@ class KnowPhishChecker(Checker):
         super().__init__(to_check_url, "")
 
     def run_check(self):
-        return KnowPhishChecker.is_known_phish(self.to_check_url, self.known_phish_db_path)
+        try:
+            return KnowPhishChecker.is_known_phish(self.to_check_url, self.known_phish_db_path)
+        except Exception as e:
+            raise CheckFailedException("failed to check if {0} is a known phish in {1}"
+                                       .format(self.to_check_url, self.known_phish_db_path)) from e
 
     @staticmethod
     def is_known_phish(url, path):
         with open(path, "r") as f:
             known_phish_urls = map(str.strip, f.readlines())
             return url.strip() in known_phish_urls
-
-
